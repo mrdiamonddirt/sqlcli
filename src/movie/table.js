f706945c7344c14c989bba9a21da34cc6855013d
RowDog <mrdiamonddirt@gmail.com>
2022-11-16 10:46:55 +0000

    created model

---
+++ src/movie/table.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../db/connection");
+
+const Movie = sequelize.define("Movie", {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  actor: {
+    type: DataTypes.STRING,
+    defaultValue: "unknown",
+  },
+  year: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  genre: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: "unknown",
+  },
+  director: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  rating: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  rottenTomatoes: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+
+module.exports = Movie;
